1cb2255d55a2034c479da377307365d3f50d75fa
Minenhle-Ngubane <minenhlengubane17@gmail.com>
2023-08-15 21:41:59 +0200

    removed railway database config

--- djlinktree/settings.py
+++ djlinktree/settings.py
@@ -97,17 +97,12 @@ WSGI_APPLICATION = 'djlinktree.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if 'RAILWAY' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.config(default=os.environ['DATABASE_URL'])
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
